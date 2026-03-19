b06e01040af6c79f1182cb7790daf5c9f084cbd2
Hugo sanchez <sanchez_hugo20@hotmail.com>
2026-03-18 21:49:50 -0500

    aula archivos básicos

---
+++ Ejercicios.py
@@ -0,0 +1,14 @@
+from os import system
+
+if system("clear") != 0: system ("cls")
+
+
+print("\nEjercicio 1: imrpime mensaje")
+print("Escribe un programa que imprima tu nombre y tu cuidas en lineas separadas.")
+
+
+
+###competa aqui
+
+print(completa aqui)
+
